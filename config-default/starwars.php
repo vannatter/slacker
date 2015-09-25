242e7145b29a31b61f3d9f5373078e6b1abca591
Dustin Vannatter <dustin@vannatter.com>
2015-09-25 16:52:18 -0400

    Adding starwars countdown plugin

---
+++ config-default/starwars.php
@@ -0,0 +1,14 @@
+<?php
+
+	$config_starwars = array(
+
+		'help_command'   => '/bot starwars',
+		'webhook_settings' => array(
+			'username' => '',
+			'icon_emoji' => '',
+			'icon_url' => ''
+		)
+
+	);
+
+?>
---
+++ plugins/starwars.php
@@ -0,0 +1,32 @@
+<?php
+	
+	class holiday extends Slacker {
+
+		protected $content;
+		protected $webhook_settings;
+		protected $config;
+		
+		function __construct() {
+			if (file_exists("config/".get_class($this).".php")) { 
+				include("config/".get_class($this).".php");
+				if (isset(${'config_'.get_class($this)})) {
+					$this->config = ${'config_'.get_class($this)};
+				}
+			}
+			parent::__construct();
+			
+			$this->webhook_setting("icon_url", "");
+			$this->webhook_setting("icon_emoji", ":clock4:");
+			$this->webhook_setting("username", "starwars-bot");
+
+			$release_date = strtotime("December 18, 2015 12:01 AM");
+			$remain = $release_date - time();
+			$days_remaining = floor($remain / 86400);
+			$hours_remaining = floor(($remain % 86400) / 3600);
+
+			$this->content = "There are " $days_remaining . " days and " . $hours_remaining . " hours remaining until Star Wars: Episode VI release!";
+		}
+
+	}
+
+?>
